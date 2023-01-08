3c29aea6ab3a66f03552bca5f8b567aceff7d854
Thomas Bender <thomas.bender82@qmail.cuny.edu>
2023-01-08 06:11:11 -0500

    fix

--- db/migrate/20230108110922_create_projects.rb
+++ db/migrate/20230108110922_create_projects.rb
@@ -2,7 +2,6 @@ class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
       t.string :name
-      t.string :name
       t.string :description
       t.string :type
 
